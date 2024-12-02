7894353746e90cb62b5ab1c22580ff454652517d
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2024-12-02 02:30:02 +0100

    Check for overflow

--- tinygrad/codegen/uopgraph.py
+++ tinygrad/codegen/uopgraph.py
@@ -118,13 +118,15 @@ def simplify_valid_load(buf:UOp, start_idx:UOp, valid:UOp) -> Optional[UOp]:
   return buf.index(idx, new_valid)
 
 @functools.lru_cache(None)
-def fast_idiv(vmax:int, idiv:int):
-  # calculate m,s such that x//d == (x*m) >> s for all 0 <= x <= vmax, taken from Hacker's Delight, Chapter 10
-  nc = ((vmax+1)//(udiv:=abs(idiv)))*udiv - 1
+def fast_idiv(vmax:int, d:int):
+  # calculate m,s such that x//d == (x*m) >> s for all 0 <= x <= vmax, adapted from Hacker's Delight, Chapter 10
+  ud = abs(d)  # we add the sign back later
+  nc = (vmax+1)//ud * ud - 1
   nbits = vmax.bit_length()
   for s in range(0, 2*nbits + 1):
-    if 2**s > nc*(udiv - 1 - (2**s - 1) % udiv):
-      m = (idiv//udiv)*(2**s + udiv - 1 - (2**s - 1) % udiv)//udiv
+    if 2**s > nc*(ud - 1 - (2**s - 1) % ud):
+      m = (d//ud) * (2**s + ud - 1 - (2**s - 1) % ud)//ud
+      if m.bit_length() + s > 32: return None
       return lambda x: x*m >> s
   assert False
 
@@ -146,10 +148,12 @@ def get_late_rewrite_patterns(ops, force_transcendental=False):
       mul << powers_of_two[const.arg] if const.arg in powers_of_two else None), # (x  * (2**y)) -> shl(x,y)
     (UPat(Ops.IDIV, dtype=dtypes.ints, src=(UPat.var("div"), UPat.cvar("const"))), lambda div, const:
       div >> powers_of_two[const.arg] if const.arg in powers_of_two else None), # (x // (2**y)) -> shr(x,y)
-    (UPat.var("x", dtypes.ints)//UPat.cvar("d", dtypes.ints, vec=False),lambda x, d: fast_idiv(x.vmax, d.arg)(x) if 0<=x.vmin else None),
-    # (UPat.var("x")//UPat.cvar("d"),lambda x, d: -fast_idiv(x.vmin, d.arg)(-x) if 0>=x.vmax else None),
-    # (UPat.var("x")//UPat.cvar("d"),lambda x, d: (0<=x).where(fast_idiv(x.vmax, d.arg)(x), -fast_idiv(x.vmin, d.arg)(-x))),
-    (UPat.var("x", dtypes.ints)%UPat.cvar("d", dtypes.ints, vec=False),lambda x, d: x - d*fast_idiv(x.vmax, d.arg)(x) if 0<=x.vmin else None)]
+    (UPat.var("x", dtypes.ints)//UPat.cvar("d", dtypes.ints, vec=False), lambda x, d:
+        f(x) if (f:=fast_idiv(x.vmax, d.arg)) is not None and 0<=x.vmin else None),
+    # (UPat.var("x", dtypes.ints)//UPat.cvar("d", dtypes.ints, vec=False), lambda x, d:
+        #  f(x) if (f:=fast_idiv(max(abs(x.vmax), abs(x.vmin)), d.arg)) is not None else None),
+    (UPat.var("x", dtypes.ints)%UPat.cvar("d", dtypes.ints, vec=False), lambda x, d:
+        x - d*f(x) if (f:=fast_idiv(x.vmax, d.arg)) is not None and 0<=x.vmin else None)]
   if Ops.NEG in ops:
     pat += [(UPat.var('x')*-1, lambda x: x.alu(Ops.NEG))]
     if Ops.SUB in ops: pat += [(UPat.var('x')+UPat.var('y').alu(Ops.NEG), lambda x,y: x.alu(Ops.SUB, y))]
